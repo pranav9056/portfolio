86a0a93037d33f920303b8a37c0d9f203a7f0be0
Pranav <pranavjain3194@gmail.com>
2017-09-17 15:49:13 -0400

    feat: navbar closes on click

--- js/active.js
+++ js/active.js
@@ -1,4 +1,10 @@
-$('.navbar-nav li').on('click',function(evt){
+$('.navbar-nav').on('click','li',function(evt){
   $('.active').toggleClass('active');
   $(evt.target).parent().toggleClass('active');
+});
+
+$(document).on("click", ".navbar", function(e) {
+    if ($(e.target).is("a") && $(e.target).attr("class") != "dropdown-toggle") {
+        $(".navbar-collapse").collapse("hide");
+    }
 });
